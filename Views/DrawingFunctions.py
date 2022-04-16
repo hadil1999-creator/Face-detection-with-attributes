e8aea7085859dc4cbbed474435ebb09be141735d
Andrik-Ts-019 <andrikdelacruz@gmail.com>
2022-04-16 18:42:35 -0500

    Refactor draw_rectangle function

--- Views/DrawingFunctions.py
+++ Views/DrawingFunctions.py
@@ -1,6 +1,7 @@
 from ast import Raise
 from Resources import ReadFunctions
-from PIL import ImageDraw, Image
+from PIL import ImageDraw, ImageFont, Image
+
 
 
 def get_rectangle(face_posicion):
@@ -12,7 +13,17 @@ def get_rectangle(face_posicion):
     return ((left, top), (right, bottom))
 
 
-def draw_rectangle(data, image):
+def draw_rectangle(data, img):
+    draw = ImageDraw.Draw(img)
+    
+    for face in range(0, len(data)):
+        dimensions = get_rectangle(data[face]['faceRectangle'])
+        draw.rectangle(dimensions, outline= 'red', width= 2)
+    
+    return img
+
+
+def draw_rectangles_in_faces(data, image):
     try:
         img = ReadFunctions.open_image(image, True)
         
@@ -23,16 +34,25 @@ def draw_rectangle(data, image):
         print(ve)
         
     else:
-        draw = ImageDraw.Draw(img)
-        
-        for face in range(0, len(data)):
-            dimensions = get_rectangle(data[face]['faceRectangle'])
-            draw.rectangle(dimensions, outline= 'red', width= 2)
+        img = draw_rectangle(data, img)
         
         return img
     
     return None
 
+""" 
+def draw_text(draw, img):
+    pass
 
-""" def draw_features(data, image):
-    pass """
+
+def draw_features(data, image):
+    img = draw_rectangle(data, image)
+    
+    if img != None:
+        draw = ImageDraw.Draw(img)
+        
+        draw.rectangle(((10, 10), (100, 60)), fill= "red")
+        
+        draw.multiline_text((15,15), "Hola gente", fill= "white")
+        
+        return img """
