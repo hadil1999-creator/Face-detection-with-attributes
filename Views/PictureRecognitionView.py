566ad020afcfc0e4500432644a46e3dad5c20e90
Andrik-Ts-019 <andrikdelacruz@gmail.com>
2022-04-17 00:30:11 -0500

    facial_recognition_with_features function added

--- Views/PictureRecognitionView.py
+++ Views/PictureRecognitionView.py
@@ -1,11 +1,27 @@
-from ast import And
-from PIL import ImageDraw
 from Controllers import FaceDetectController
-from Views import DrawingFunctions
+from Resources import DrawingFunctions
+
 
 
 def picture_face_recognition(image_name):
     data, image = FaceDetectController.face_identify(image_name)
     
     if (data != None) and (image != None):
-        DrawingFunctions.draw_image(data, image)
+        picture = DrawingFunctions.draw_rectangles_in_faces(data, image)
+        
+        if picture != None:
+            picture.show()
+            picture.close()
+
+
+def facial_recognition_with_features(image_name):
+    data, image = FaceDetectController.face_identify(image_name)
+    
+    if (data != None) and (image != None):
+        picture = DrawingFunctions.draw_features(data, image)
+        
+        if picture != None:
+            picture.show()
+            #picture.save("geeks.jpg")
+            picture.close()
+        
