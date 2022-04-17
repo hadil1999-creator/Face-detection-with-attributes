01447e208227c743239e79358e3b23c41dd27f06
Andrik-Ts-019 <andrikdelacruz@gmail.com>
2022-04-17 00:40:37 -0500

    Temporal menu

--- run.py
+++ run.py
@@ -1,7 +1,27 @@
 from Views import PictureRecognitionView
 
+
 def run():
-    PictureRecognitionView.picture_face_recognition("Messi_3.jpg")
+    try:
+        menu = '''
+        1.- Picture face recognition
+        2.- Facial recognition with features
+        
+        Selection: '''
+        x = int(input(menu))
+    
+    except Exception:
+        print(f"\nError in the input")
+    
+    else:
+        if x == 1:
+            PictureRecognitionView.picture_face_recognition("OmarTobias_3.jpg")
+        
+        elif x == 2:
+            PictureRecognitionView.facial_recognition_with_features("OmarTobias_3.jpg")
+        
+        else:
+            print(f"\nSin accion")
 
 
 if __name__ == "__main__":
