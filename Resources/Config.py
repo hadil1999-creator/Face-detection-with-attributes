3037640e904e6c5aa4179b595d99f18a91df44b9
Andrik-Ts-019 <andrikdelacruz@gmail.com>
2022-04-23 23:27:17 -0500

    Variable NAME_DISPLAY added

--- Resources/Config.py
+++ Resources/Config.py
@@ -6,3 +6,5 @@ CONNECTION_FILE_NAME = 'AK.txt'
 # Path default to search and read images: execution_path
 DEFAULT_IMAGE_PATH = os.getcwd() + '\\' + 'Images'
 
+# Name display default
+NAME_DISPLAY = True
