204beaf4cd71f9b315bdef3e536e355bbc9aef77
Andrik-Ts-019 <andrikdelacruz@gmail.com>
2022-04-23 20:10:37 -0500

    FaceIdentifyController file created

---
+++ Controllers/FaceIdentifyController.py
@@ -0,0 +1,11 @@
+import uuid
+
+def identify_face():
+    try:
+        if True:
+            raise NotImplementedError(f"\nNo implement identify_face yet")
+    
+    except NotImplementedError as nie:
+        print(nie)
+    
+    return None
