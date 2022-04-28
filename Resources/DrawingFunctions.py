f969b5c80be2dfe283cf8a605f7e4e87d9c3fb21
Andrik-Ts-019 <andrikdelacruz@gmail.com>
2022-04-27 20:34:37 -0500

    Added attribute rectangle dynamic height

--- Resources/DrawingFunctions.py
+++ Resources/DrawingFunctions.py
@@ -11,26 +11,33 @@ def draw_rectangle(data, img, in_attributes= False, with_name= False):
         dictionary = FaceDetectConfig.param()
         
         attributes = ""
+        n_attributes = 0
         
         face_attributes_params = ReadFunctions.get_dictionary_text(dictionary, 'returnFaceAttributes', ',')
         
         for face in data:
             dimensions = ReadFunctions.get_rectangle(face['faceRectangle'])
             
-            draw.rectangle((dimensions[0][0], dimensions[1][1], dimensions[0][0]+80, dimensions[1][1]+60),
-                           fill= "red")
-            
             if with_name:
                 attributes += ReadFunctions.get_name(dictionary['returnFaceId'], face['faceId'])
             
             if face_attributes_params != None:
                 attributes += ReadFunctions.get_face_data(face['faceAttributes'])
+                
+                auxiliar = attributes.split('.\n')
+                
+                n_attributes += len(auxiliar)
+            
+            draw.rectangle((dimensions[0][0], dimensions[1][1], dimensions[0][0]+90, dimensions[1][1]+13*n_attributes),
+                           fill= "red")
             
             draw.multiline_text((dimensions[0][0]+2, dimensions[1][1]+2),
                                 text= attributes,
                                 fill= "white")
             
             attributes = ""
+            
+            n_attributes = 0
         
     else:
         for face in data:
