930a28c737a4698c90df53ff60b07e196bc800f4
Andrik-Ts-019 <andrikdelacruz@gmail.com>
2022-04-29 18:34:27 -0500

    put function added

--- Controllers/RequestsFunctions.py
+++ Controllers/RequestsFunctions.py
@@ -11,3 +11,14 @@ def post_Http(url, headers, params, body):
         print(f"\nError in the post function.")
         
     return response
+
+def put_Http(url, headers, body):
+    response = None
+    
+    try:
+        response = requests.request('POST', url, headers = headers, data = body)
+                
+    except Exception:
+        print(f"\nError in the put function.")
+        
+    return response
