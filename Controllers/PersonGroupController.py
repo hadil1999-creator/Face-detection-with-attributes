c6a25fd3bf0bf7670402f83fd68273de12cbbba0
Andrik-Ts-019 <andrikdelacruz@gmail.com>
2022-04-29 16:42:02 -0500

    PersonGroupController created

---
+++ Controllers/PersonGroupController.py
@@ -0,0 +1,17 @@
+from Resources import ReadFunctions
+import requests
+
+
+def create_person_froup_id():
+    person_group_info = ""
+    
+    try:
+        PERSON_GROUP_ID, TARGET_PERSON_GROUP_ID = ReadFunctions.get_person_group_id()
+        
+    except Exception:
+        pass
+    
+    else:
+        pass
+        
+
